227ed73d579133d8864fbe2d59665b6af484cf6a
LR222222 <er339467@truro-penwith.ac.uk>
2023-10-16 12:12:48 +0100

    Added a live countdown to the event
    Using a script there is now a countdown to the date of the 5th of January

--- info.php
+++ info.php
@@ -20,7 +20,7 @@ include_once 'head.php';
         <h1 class="second-title">Dates</h1>
         <p>Tickets go on sale the 1st November 2023!</p>
         <p>Festival dates: 5th - 7th January 2024</p>
-        
+        <?php include_once 'countdown.php' ?>
     </div>
     <div class="col section">
         <h1 class="second-title">Where to Go</h1>
