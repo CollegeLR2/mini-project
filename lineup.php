0ff9e73231484c85a75aab3e18174aca98a492a4
LR222222 <er339467@truro-penwith.ac.uk>
2023-11-03 14:37:36 +0000

    Make lineup mobile accessible

--- lineup.php
+++ lineup.php
@@ -18,35 +18,35 @@ include_once 'head.php';
     <div class="section" style="width: 66%; margin: auto;">
         <div class="col-12" style="margin: auto;">
             <a href="soad.php">
-            <img src="soad-logo.png" alt="System of a Down">
+            <img src="soad-logo.png" alt="System of a Down" style="width: 60%">
             </a>
         </div>
         <div class="row align-items-center">
             <div class="col-6">
                 <a href="ghost.php">
-                <img src="ghost-logo.png" alt="Ghost" style="width: 50%">
+                <img src="ghost-logo.png" alt="Ghost" style="width: 80%">
                 </a>
             </div>
             <div class="col-6">
                 <a href="avatar.php">
-                <img src="avatar-logo.png" alt="Avatar" style="width: 50%">
+                <img src="avatar-logo.png" alt="Avatar" style="width: 80%">
                 </a>
             </div>
         </div>
         <div class="row align-items-center">
             <div class="col-4">
                 <a href="ink.php">
-                <img src="ink-logo.png" alt="Ice Nine Kills" style="width: 50%">
+                <img src="ink-logo.png" alt="Ice Nine Kills" style="width: 100%">
                 </a>
             </div>
             <div class="col-4">
                 <a href="miw.php">
-                <img src="miw-logo.png" alt="Motionless in White" style="width: 50%">
+                <img src="miw-logo.png" alt="Motionless in White" style="width: 100%">
                 </a>
             </div>
             <div class="col-4">
                 <a href="etf.php">
-                <img src="etf-logo.png" alt="Escape the Fate" style="width: 50%">
+                <img src="etf-logo.png" alt="Escape the Fate" style="width: 100%">
                 </a>
             </div>
         </div>
